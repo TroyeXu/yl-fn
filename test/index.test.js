8eedbd0799ff08fb8c0bb8d8a2b6f6fed7915a9c
troye <troye@forfuns.net>
2020-09-22 15:17:42 +0800

    fix: remove redundant comments

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,11 @@
-const { setLocalStorage, getLocalStorage } = '../src/index.js'
-setLocalStorage('account', 'helloAccount')
-const ls = getLocalStorage('account')
-console.log('utils-get', ls)
 /** @format */
 
-test('two plus two is four', () => {
+// const { setLocalStorage, getLocalStorage } = '../src/index.js'
+// setLocalStorage('account', 'helloAccount')
+// const ls = getLocalStorage('account')
+// console.log('utils-get', ls)
+// /** @format */
+
+test("two plus two is four", () => {
   expect(2 + 2).toBe(4)
 })
