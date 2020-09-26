37cbfa431b5b4d645eb8df14172ca128d196b22c
troye <troye@forfuns.net>
2020-09-26 16:55:41 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+/** @format */
+/**
+ * 等待
+ */
+export const sleep = function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
 /**
  * 存儲localStorage
- *
- * @format
  */
-
-export const setLocalStorage = function (name, content) {
+export const setLocalStorage = (name, content) => {
   name = "forfuns_" + name
   if (!name) return
   if (typeof content !== "string") {
@@ -32,13 +36,13 @@ export const getLocalStorage = (name) => {
  * 刪除localStorage
  */
 export const removeLocalStorage = (name) => {
-  name = "yl_" + name
+  name = "forfuns_" + name
   if (!name) return
   window.localStorage.removeItem(name)
 }
 
 export const removeAllLocalStorage = () => {
-  let name = "yl"
+  let name = "forfuns"
   for (var key in localStorage) {
     if (key.indexOf("lang") != -1) return
     if (key.indexOf(name) != -1) {
@@ -46,3 +50,7 @@ export const removeAllLocalStorage = () => {
     }
   }
 }
+
+// time
+
+// 數學換算
